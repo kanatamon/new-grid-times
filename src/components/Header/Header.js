9155c059b37316a1d687a3e3aa14058a75a44d76
kanatamon <ponde.moonlord@gmail.com>
2021-08-03 12:10:34 +0700

    Finish header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import { Menu, Search, User } from 'react-feather';
+import React from 'react'
+import styled from 'styled-components/macro'
+import { Menu, Search, User } from 'react-feather'
 
-import { QUERIES } from '../../constants';
+import { QUERIES } from '../../constants'
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from '../MaxWidthWrapper'
+import Logo from '../Logo'
+import Button from '../Button'
 
 const Header = () => {
   return (
@@ -29,22 +29,42 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <DesktopHeaderLeft>
+          <ActionGroup>
+            <button>
+              <Search size={24} />
+            </button>
+            <button>
+              <Menu size={24} />
+            </button>
+          </ActionGroup>
+        </DesktopHeaderLeft>
         <Logo />
+        <DesktopHeaderRight>
+          <CTAGroup>
+            <Button>Subscribe</Button>
+            <CTALink href="#">Already a subscriber?</CTALink>
+          </CTAGroup>
+        </DesktopHeaderRight>
       </MainHeader>
     </header>
-  );
-};
+  )
+}
 
 const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
-`;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: none;
+  }
+`
 
 const Row = styled(MaxWidthWrapper)`
   display: flex;
   justify-content: space-between;
-`;
+`
 
 const ActionGroup = styled.div`
   display: flex;
@@ -57,14 +77,50 @@ const ActionGroup = styled.div`
   svg {
     display: block;
   }
-`;
+`
 
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
-  align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
-`;
 
-export default Header;
+  @media ${QUERIES.desktopAndUp} {
+    display: grid;
+    grid-template-columns: 1fr auto 1fr;
+    align-items: center;
+  }
+`
+
+const DesktopHeaderLeft = styled.div`
+  display: none;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: revert;
+  }
+`
+
+const DesktopHeaderRight = styled.div`
+  display: none;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: revert;
+    margin-top: auto;
+    margin-left: auto;
+  }
+`
+
+const CTAGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+`
+
+const CTALink = styled.a`
+  font-size: ${14 / 16}rem;
+  font-style: italic;
+  text-decoration: underline;
+`
+
+export default Header
