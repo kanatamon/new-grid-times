49b24bff12d43c86ccd79e253d3b7c61ac7bcd6f
kanatamon <ponde.moonlord@gmail.com>
2021-08-04 12:19:47 +0700

    Simplify by toggle float & margin

--- src/components/OpinionStory/OpinionStory.js
+++ src/components/OpinionStory/OpinionStory.js
@@ -17,16 +17,7 @@ const OpinionStory = ({ id, title, author, avatar }) => {
 }
 
 const Wrapper = styled.article`
-  display: flex;
-  flex-direction: row-reverse;
-  justify-content: space-between;
-  gap: 32px;
   color: var(--color-gray-900);
-
-  @media ${QUERIES.tabletOnly} {
-    flex-direction: column;
-    gap: 8px;
-  }
 `
 
 const Avatar = styled.img`
@@ -35,6 +26,13 @@ const Avatar = styled.img`
   height: 48px;
   border-radius: 50%;
   object-fit: cover;
+  float: right;
+  margin-left: 16px;
+
+  @media ${QUERIES.tabletOnly} {
+    float: revert;
+    margin-left: revert;
+  }
 `
 
 const AuthorName = styled.p`
